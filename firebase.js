1c1f3f870628ef78d2540f25b7556523e0e4443a
Yash90234 <128340739+Yash90234@users.noreply.github.com>
2023-03-20 08:47:53 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD4oIxcwpDqbV6-xCYXQX-YAy_UOLRRXOY",
+    authDomain: "auth-2bef8.firebaseapp.com",
+    databaseURL: "https://auth-2bef8-default-rtdb.firebaseio.com",
+    projectId: "auth-2bef8",
+    storageBucket: "auth-2bef8.appspot.com",
+    messagingSenderId: "380500500350",
+    appId: "1:380500500350:web:f12aeb30c4b1e3b9561d2f"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
